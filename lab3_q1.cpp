0a42c072cb1b81878bab0b2f62ebb41b5f62e414
diprupasaha <42206483+diprupasaha@users.noreply.github.com>
2018-08-17 11:37:22 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,31 @@
+//add library
+
+#include <iostream>
+using namespace std;
+
+
+//starting the function
+
+int main(){
+
+//declaring variables
+
+int INT=7;
+float FLOAT=1.4;
+char CHAR='V';
+bool BOOL=true;
+
+                                                                                                                                                                                                                                                                                                              
+//code to print the output in the terminal
+
+cout <<INT<<" is integer data type"<<endl;
+cout <<FLOAT<<" is float data type"<<endl;
+cout <<CHAR<<" is character data type"<<endl;
+cout <<BOOL<<" is Boolean data type"<<endl;
+
+cout <<"The size of integer "<<INT<<" is "<<sizeof(INT)<<endl;
+cout <<"The size of float "<<FLOAT<<" is "<<sizeof(FLOAT)<<endl;
+cout <<"The size of character "<<CHAR<<" is "<<sizeof(CHAR)<<endl;
+cout <<"The size of boolean "<<BOOL<<" is "<<sizeof(BOOL)<<endl;
+return 7;
+}
