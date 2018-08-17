c3b17a89a77ab3c9f7a318050e1a2ddd113adb32
diprupasaha <42206483+diprupasaha@users.noreply.github.com>
2018-08-17 11:56:20 +0530

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,35 @@
+//add library
+
+#include <iostream>
+using namespace std;
+
+
+//starting the function
+
+int main(){
+
+//declaring variables
+
+int INT1=7;
+int INT2=9;
+
+//Performing the arithmetic operators
+
+int Sum=INT1+INT2;
+int Diff=INT2-INT1;
+int Mul=INT1*INT2;
+int Div=INT2/INT1;
+int Rem=INT2%INT1;
+
+                                                                                                                                                                                                                                                                                                              
+//code to print the output in the terminal
+
+cout <<"The first integer is "<<INT2<<endl;
+cout <<"The second integer is "<<INT1<<endl;
+cout <<"Their sum is "<<Sum<<endl;
+cout <<"Their difference is "<<Diff<<endl;
+cout <<"Their product is "<<Mul<<endl;
+cout <<"Their quotient is "<<Div<<endl;
+cout <<"Their remainder is "<<Rem<<endl;
+return 7;
+}
